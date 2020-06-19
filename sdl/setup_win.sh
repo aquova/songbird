e2ac9ffc738a71043093572aff56750e28e151ee
aquova <abrick912@gmail.com>
2020-06-18 19:15:12 -0500

    Switch to bundled SDL2 crate
    Switches the SDL2 crate type to "bundled", which will compile a
    version of SDL2 during the compilation process.

--- sdl/setup_win.sh
+++ sdl/setup_win.sh
@@ -6,5 +6,3 @@
 
 sudo apt install -y gcc-mingw-w64-x86-64
 rustup target add x86_64-pc-windows-gnu
-cp -r lib/SDL2-2.0.12/x86_64-w64-mingw32/lib/* ~/.rustup/toolchains/stable-x86_64-unkown-linux-gnu/lib/rustlib/x86_64-pc-windows-gnu/lib
-cp lib/SDL2-2.0.12/x86_64-w64-mingw32/bin/SDL2.dll .
