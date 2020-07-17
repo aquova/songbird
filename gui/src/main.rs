e9ca745dd54772863127ebaa7828c0baf032130d
aquova <abrick912@gmail.com>
2020-07-17 15:26:11 -0500

    Add Imgui layer for GUI elements

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,31 +2,17 @@
 // Austin Bricker 2019-2020
 
 // Includes
+mod ui;
+
+#[macro_use]
+extern crate imgui;
+
 use songbird_core::cpu::Cpu;
-use songbird_core::io::Buttons;
-use songbird_core::utils::{DISP_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH};
-
-use glium::glutin::ContextBuilder;
-use glium::glutin::dpi::LogicalSize;
-use glium::glutin::event::{ElementState, Event, KeyboardInput, WindowEvent, VirtualKeyCode};
-use glium::glutin::event_loop::{ControlFlow, EventLoop};
-use glium::glutin::window::WindowBuilder;
-
-use glium::BlitTarget;
-use glium::Display;
-use glium::Surface;
-use glium::texture::{MipmapsOption, RawImage2d, Texture2d, UncompressedFloatFormat};
-use glium::uniforms::MagnifySamplerFilter;
+use crate::ui::ImguiSystem;
 
 use std::{env, process};
 use std::fs::{File, OpenOptions};
-use std::io::prelude::*;
 use std::io::Read;
-
-// Constants
-const SCALE: usize = 5;
-const WINDOW_WIDTH: u32 = (SCREEN_WIDTH * SCALE) as u32;
-const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT * SCALE) as u32;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -43,121 +29,11 @@ fn main() {
     load_battery_save(&mut gb, &filename);
     let title = gb.get_title();
 
-    // Setup window
-    let event_loop = EventLoop::new();
-    let wb = WindowBuilder::new().with_inner_size(LogicalSize {
-                    width: WINDOW_WIDTH,
-                    height: WINDOW_HEIGHT,
-                }).with_title(title);
-    let cb = ContextBuilder::new().with_vsync(true);
-    let display = Display::new(wb, cb, &event_loop).unwrap();
-    let dest_texture = Texture2d::empty_with_format(
-        &display,
-        UncompressedFloatFormat::U8U8U8U8,
-        MipmapsOption::NoMipmap,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT
-    ).unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-                // Exit program if specified
-                *control_flow = ControlFlow::Exit;
-            },
-            Event::WindowEvent { event: WindowEvent::KeyboardInput {
-                input: KeyboardInput { virtual_keycode: Some(keycode), state, ..}, ..}, ..} => {
-                    // Send keyboard inputs to emulator core
-                    if let Some(btn) = key2btn(keycode) {
-                        gb.toggle_button(btn, state == ElementState::Pressed);
-                    }
-            },
-            Event::MainEventsCleared => {
-                tick_until_draw(&mut gb, &filename);
-                let disp_arr = gb.render();
-                draw_screen(&disp_arr, &display, &dest_texture);
-            }
-            _ => {}
-        }
-    });
+    // Setup window and GUI
+    let is = ImguiSystem::new(title);
+    is.main_loop(gb, filename);
 }
 
-/// ```
-/// Tick until draw
-///
-/// Repeatedly runs until it is time to render a frame
-///
-/// Inputs:
-///     Game Boy CPU (&Cpu)
-///     Filename of game ROM (&String)
-/// ```
-fn tick_until_draw(gb: &mut Cpu, filename: &String) {
-    let mut draw_time = false;
-    while !draw_time {
-        draw_time = gb.tick();
-
-        if gb.is_battery_dirty() {
-            write_battery_save(gb, &filename);
-        }
-    }
-}
-
-/// ```
-/// Draw screen
-///
-/// Renders pixel data onto window
-///
-/// Inputs:
-///     Pixel data ([u8])
-///     Display to render to (&Display)
-///     Texture to place pixels on (&Texture2d)
-/// ```
-fn draw_screen(data: &[u8; DISP_SIZE], display: &Display, dest: &Texture2d) {
-    // Blitting implementation from here: https://github.com/avp/gb-rust/
-    let image = RawImage2d::from_raw_rgba_reversed(&data.to_vec(), (SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32));
-
-    let texture = Texture2d::new(display, image).unwrap();
-    let dest_rect = BlitTarget {
-        left: 0,
-        bottom: 0,
-        width: WINDOW_WIDTH as i32,
-        height: WINDOW_HEIGHT as i32,
-    };
-
-    texture.as_surface().blit_whole_color_to(
-        &dest.as_surface(),
-        &dest_rect,
-        MagnifySamplerFilter::Nearest
-    );
-
-    let target = display.draw();
-    dest.as_surface().fill(&target, MagnifySamplerFilter::Nearest);
-    target.finish().unwrap();
-}
-/// ```
-/// Key to Button
-///
-/// Converts keycode into GameBoy button
-///
-/// Input:
-///     SDL keybode keycode (VirtualKeyCode)
-///
-/// Output:
-///     Gameboy button (Option<Buttons>)
-/// ```
-fn key2btn(key: VirtualKeyCode) -> Option<Buttons> {
-    match key {
-        VirtualKeyCode::Down =>    { Some(Buttons::Down)   },
-        VirtualKeyCode::Up =>      { Some(Buttons::Up)     },
-        VirtualKeyCode::Right =>   { Some(Buttons::Right)  },
-        VirtualKeyCode::Left =>    { Some(Buttons::Left)   },
-        VirtualKeyCode::Return =>  { Some(Buttons::Start)  },
-        VirtualKeyCode::Back =>    { Some(Buttons::Select) },
-        VirtualKeyCode::X =>       { Some(Buttons::A)      },
-        VirtualKeyCode::Z =>       { Some(Buttons::B)      },
-        _ =>                       { None                  }
-    }
-}
 
 /// ```
 /// Load ROM
@@ -199,26 +75,5 @@ fn load_battery_save(gb: &mut Cpu, gamename: &str) {
             f.unwrap().read_to_end(&mut battery_ram).expect("Error reading external RAM");
             gb.write_ext_ram(&battery_ram);
         }
-    }
-}
-
-/// ```
-/// Write Battery save
-///
-/// Updates save file to latest contents of battery RAM
-///
-/// Inputs:
-///     Game Boy CPU object (Cpu)
-///     Name of ROM file (&str)
-/// ```
-fn write_battery_save(gb: &mut Cpu, gamename: &str) {
-    if gb.has_battery() {
-        let ram_data = gb.get_ext_ram();
-        let mut filename = gamename.to_owned();
-        filename.push_str(".sav");
-
-        let mut file = OpenOptions::new().write(true).create(true).open(filename).expect("Error opening save file");
-        file.write(ram_data).unwrap();
-        gb.clean_battery_flag();
     }
 }
