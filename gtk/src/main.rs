06cdc7731af47617baa473406c9e980a083376bc
aquova <abrick912@gmail.com>
2021-01-08 14:34:52 -0600

    Basic functionality working, except for rendering

--- gtk/src/main.rs
+++ gtk/src/main.rs
@@ -1,200 +1,60 @@
 // Songbird GTK desktop build
-// Austin Bricker 2020
+// Austin Bricker 2020-2021
 
 mod menubar;
+mod ui;
+
+use crate::ui::*;
+
+use songbird_core::cpu::Cpu;
 
 use std::cell::RefCell;
-use std::env::args;
 use std::fs::{File, OpenOptions};
 use std::io::{prelude::*, Read};
 use std::path::PathBuf;
 use std::rc::Rc;
-
-use songbird_core::cpu::Cpu;
-use songbird_core::io::Buttons;
-use songbird_core::ppu::palette::Palette;
-use songbird_core::utils::{SCREEN_HEIGHT, SCREEN_WIDTH};
-use crate::menubar::EmuMenubar;
-
-use gdk::keys::Key;
-use gio::prelude::*;
-use gtk::prelude::*;
-use gtk::{AccelFlags, AccelGroup, Application, ApplicationWindow, FileChooserAction, FileChooserDialog, FileFilter, GLArea, Orientation, WindowPosition};
-
-#[cfg(feature = "debug")]
-use coredump::register_panic_handler;
-
-const SCALE: usize = 5;
-const WINDOW_WIDTH: usize = SCREEN_WIDTH * SCALE;
-const WINDOW_HEIGHT: usize = SCREEN_HEIGHT * SCALE;
-
-struct App {
-    window: ApplicationWindow,
-    accel_group: AccelGroup,
-    menubar: EmuMenubar,
-    gb: Rc<RefCell<Cpu>>,
-    gl: GLArea,
-}
-
-impl App {
-    pub fn new(app: &Application) -> Self {
-        let gb = Rc::new(RefCell::new(Cpu::new()));
-
-        let window = ApplicationWindow::new(app);
-        window.set_title("Songbird");
-        window.set_position(WindowPosition::Center);
-        window.set_size_request(WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32);
-
-        // Add items to window
-        let v_box = gtk::Box::new(Orientation::Vertical, 0);
-        let menubar = EmuMenubar::new();
-        let gl = GLArea::new();
-        v_box.pack_start(&menubar.menubar, false, false, 0);
-        v_box.pack_start(&gl, true, true, 0);
-        window.add(&v_box);
-
-        let accel_group = AccelGroup::new();
-        window.add_accel_group(&accel_group);
-
-        window.show_all();
-
-        let us = Self {
-            window,
-            accel_group,
-            menubar,
-            gb,
-            gl,
-        };
-
-        us.connect_events();
-        us
-    }
-
-    fn connect_events(&self) {
-        self.connect_quit();
-        self.connect_open();
-        self.connect_keypress();
-        self.connect_keyrelease();
-    }
-
-    fn connect_quit(&self) {
-        let window = self.window.clone();
-        self.menubar.quit_btn.connect_activate(move |_| window.close());
-        let (quit_key, quit_modifier) = gtk::accelerator_parse("<Primary>Q");
-        self.menubar.quit_btn.add_accelerator("activate", &self.accel_group, quit_key, quit_modifier, AccelFlags::VISIBLE);
-    }
-
-    fn connect_open(&self) {
-        let window = self.window.clone();
-        let gb = self.gb.clone();
-        self.menubar.open_btn.connect_activate(move |_| {
-            let filename = show_open_dialog(&window);
-            if let Some(f) = filename {
-                setup_emu(&mut gb.borrow_mut(), &f, false);
-            }
-        });
-        let (open_key, open_modifier) = gtk::accelerator_parse("<Primary>O");
-        self.menubar.open_btn.add_accelerator("activate", &self.accel_group, open_key, open_modifier, AccelFlags::VISIBLE);
-    }
-
-    fn connect_keypress(&self) {
-        let window = self.window.clone();
-        let gb = self.gb.clone();
-        window.connect_key_press_event(move|_, evt| {
-            let mut key = evt.get_keyval();
-            *key = gdk::keyval_to_upper(*key);
-            if let Some(btn) = key2btn(key) {
-                gb.borrow_mut().toggle_button(btn, true);
-            }
-
-            Inhibit(false)
-        });
-    }
-
-    fn connect_keyrelease(&self) {
-        let window = self.window.clone();
-        let gb = self.gb.clone();
-        window.connect_key_release_event(move|_, evt| {
-            let mut key = evt.get_keyval();
-            *key = gdk::keyval_to_upper(*key);
-            if let Some(btn) = key2btn(key) {
-                gb.borrow_mut().toggle_button(btn, false);
-            }
-
-            Inhibit(false)
-        });
-    }
-}
+use std::sync::mpsc::channel;
+use std::thread;
 
 fn main() {
-    #[cfg(feature = "debug")]
-    register_panic_handler().unwrap();
+    let mut gb = Cpu::new();
+    let filename: Rc<RefCell<Option<PathBuf>>> = Rc::new(RefCell::new(None));
+    let (ui_to_gb_sender, ui_to_gb_receiver) = channel::<UiAction>();
+    let (gb_to_ui_sender, gb_to_ui_receiver) = channel::<CoreAction>();
 
-    let application = Application::new(Some("com.github.aquova.songbird"), Default::default()).expect("Initialization failed");
-    application.connect_activate(|application| {
-        let _app = App::new(application);
+    let ui_thread = thread::spawn(move || {
+        create_ui(ui_to_gb_sender, gb_to_ui_receiver);
     });
-    application.run(&args().collect::<Vec<_>>());
-}
 
-fn show_open_dialog(parent: &ApplicationWindow) -> Option<PathBuf> {
-    let mut file = None;
-    let dialog = FileChooserDialog::new(Some("Select a Game Boy ROM"), Some(parent), FileChooserAction::Open);
-    let filter = FileFilter::new();
-    filter.add_mime_type("application/x-gameboy-rom");
-    filter.add_mime_type("application/x-gameboy-color-rom");
-    filter.set_name(Some("Game Boy ROM files"));
-    dialog.add_filter(&filter);
-    dialog.add_buttons(&[
-        ("Open", gtk::ResponseType::Ok),
-        ("Cancel", gtk::ResponseType::Cancel),
-    ]);
+    // Main loop
+    loop {
+        if let Ok(evt) = ui_to_gb_receiver.try_recv() {
+            match evt {
+                UiAction::Quit => break,
+                UiAction::Load(f) => {
+                    println!("Loading {:?}", f);
+                    setup_emu(&mut gb, &f, false);
+                    filename.borrow_mut().replace(f);
+                },
+                UiAction::BtnPress(btn) => {
+                    println!("Push");
+                    gb.toggle_button(btn, true);
+                },
+                UiAction::BtnRelease(btn) => {
+                    println!("Unpush");
+                    gb.toggle_button(btn, false);
+                }
+            }
+        }
 
-    let result = dialog.run();
-    if result == gtk::ResponseType::Ok {
-        file = dialog.get_filename();
+        if let Some(f) = filename.borrow().as_ref() {
+            tick_until_draw(&mut gb, &f);
+            let disp_arr = gb.render();
+            gb_to_ui_sender.send(CoreAction::Render(disp_arr)).unwrap();
+        }
     }
-    dialog.close();
-
-    file
+    ui_thread.join().unwrap();
 }
-
-// fn load_shader(display: &Display, shad: Shaders) -> Program {
-//     match shad {
-//         Shaders::None => {
-//             Program::from_source(
-//                 display,
-//                 include_str!("shaders/base.vert"),
-//                 include_str!("shaders/none.frag"),
-//                 None
-//             ).unwrap()
-//         },
-//         Shaders::CRT => {
-//             Program::from_source(
-//                 display,
-//                 include_str!("shaders/base.vert"),
-//                 include_str!("shaders/crt.frag"),
-//                 None
-//             ).unwrap()
-//         },
-//         Shaders::AsciiMono => {
-//             Program::from_source(
-//                 display,
-//                 include_str!("shaders/base.vert"),
-//                 include_str!("shaders/ascii-1bit.frag"),
-//                 None
-//             ).unwrap()
-//         },
-//         Shaders::AsciiColor => {
-//             Program::from_source(
-//                 display,
-//                 include_str!("shaders/base.vert"),
-//                 include_str!("shaders/ascii-color.frag"),
-//                 None
-//             ).unwrap()
-//         },
-//     }
-// }
 
 /// ```
 /// Tick until draw
@@ -206,44 +66,14 @@ fn show_open_dialog(parent: &ApplicationWindow) -> Option<PathBuf> {
 ///     Filename of game ROM (&PathBuf)
 /// ```
 fn tick_until_draw(gb: &mut Cpu, filename: &PathBuf) {
-    loop {
-        let draw_time = gb.tick();
-
-        if draw_time {
-            break;
-        }
-    }
+    // Keep ticking until returns true, indicating time to render
+    while !gb.tick() {}
 
     // Limiting saving battery state to only once per frame.
     // Doing it every tick is both overkill and causes some unknown issue on
     // Windows which traps us in an infinite loop on this frame
     if gb.is_battery_dirty() {
         write_battery_save(gb, &filename);
-    }
-}
-
-/// ```
-/// Key to Button
-///
-/// Converts keycode into GameBoy button
-///
-/// Input:
-///     GDK keybode keycode (Key)
-///
-/// Output:
-///     Gameboy button (Option<Buttons>)
-/// ```
-fn key2btn(key: Key) -> Option<Buttons> {
-    match key {
-        gdk::keys::constants::Down =>    Some(Buttons::Down),
-        gdk::keys::constants::Up =>      Some(Buttons::Up),
-        gdk::keys::constants::Right =>   Some(Buttons::Right),
-        gdk::keys::constants::Left =>    Some(Buttons::Left),
-        gdk::keys::constants::Return =>  Some(Buttons::Start),
-        gdk::keys::constants::Back =>    Some(Buttons::Select),
-        gdk::keys::constants::X =>       Some(Buttons::A),
-        gdk::keys::constants::Z =>       Some(Buttons::B),
-        _ =>                             None
     }
 }
 
---
+++ gtk/src/ui.rs
@@ -0,0 +1,194 @@
+use crate::menubar::EmuMenubar;
+
+use songbird_core::io::Buttons;
+use songbird_core::utils::{SCREEN_HEIGHT, SCREEN_WIDTH, DISP_SIZE};
+
+use std::path::PathBuf;
+use std::sync::mpsc::{Receiver, Sender};
+
+use gdk::keys::Key;
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::{AccelFlags, AccelGroup, Application, ApplicationWindow, DrawingArea, FileChooserAction, FileChooserDialog, FileFilter, Orientation, WindowPosition};
+
+const SCALE: usize = 5;
+const WINDOW_WIDTH: usize = SCREEN_WIDTH * SCALE;
+const WINDOW_HEIGHT: usize = SCREEN_HEIGHT * SCALE;
+
+pub enum UiAction {
+    Quit,
+    Load(PathBuf),
+    BtnPress(Buttons),
+    BtnRelease(Buttons),
+}
+
+pub enum CoreAction {
+    Render([u8; DISP_SIZE]),
+}
+
+pub fn create_ui(ui_to_gb: Sender<UiAction>, gb_to_ui: Receiver<CoreAction>) {
+    gtk::init().unwrap();
+    let app = Application::new(Some("com.github.aquova.songbird"), Default::default()).expect("Initialization failed");
+
+    let window = ApplicationWindow::new(&app);
+    window.set_title("Songbird");
+    window.set_position(WindowPosition::Center);
+    window.set_size_request(WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32);
+
+    // Add items to window
+    let v_box = gtk::Box::new(Orientation::Vertical, 0);
+    let menubar = EmuMenubar::new();
+    let drawing_area = DrawingArea::new();
+    v_box.pack_start(&menubar.menubar, false, false, 0);
+    v_box.pack_start(&drawing_area, true, true, 0);
+    window.add(&v_box);
+
+    let accel_group = AccelGroup::new();
+    window.add_accel_group(&accel_group);
+
+    window.show_all();
+
+    connect_quit(&window, &menubar, &accel_group, &ui_to_gb);
+    connect_open(&window, &menubar, &accel_group, &ui_to_gb);
+    connect_keypress(&window, &ui_to_gb);
+    connect_keyrelease(&window, &ui_to_gb);
+
+    app.connect_activate(move |app| {
+        app.add_window(&window);
+    });
+
+    if let Ok(evt) = gb_to_ui.try_recv() {
+        match evt {
+            CoreAction::Render(data) => {
+                // drawing_area.connect_draw(move |_, cr| {
+                //     let img = ImageSurface::create_for_data(
+                //         data,
+                //         Format::A8,
+                //         WINDOW_WIDTH as i32,
+                //         WINDOW_HEIGHT as i32,
+                //         Format::A8.stride_for_width(WINDOW_WIDTH as u32).unwrap(),
+                //     ).unwrap();
+
+                //     let pattern = SurfacePattern::create(&img);
+                //     pattern.set_filter(Filter::Nearest);
+                //     cr.set_source(&pattern);
+                //     cr.paint();
+
+                //     Inhibit(false)
+                // });
+            }
+        }
+    }
+
+    app.run(&[]);
+    ui_to_gb.send(UiAction::Quit).unwrap();
+}
+
+fn connect_quit(
+    window: &ApplicationWindow,
+    menubar: &EmuMenubar,
+    accel_group: &AccelGroup,
+    ui_to_gb: &Sender<UiAction>
+) {
+    let window = window.clone();
+    let ui_to_gb = ui_to_gb.clone();
+    menubar.quit_btn.connect_activate(move |_| {
+        ui_to_gb.send(UiAction::Quit).unwrap();
+        window.close()
+    });
+    let (quit_key, quit_modifier) = gtk::accelerator_parse("<Primary>Q");
+    menubar.quit_btn.add_accelerator("activate", accel_group, quit_key, quit_modifier, AccelFlags::VISIBLE);
+}
+
+fn connect_open(
+    window: &ApplicationWindow,
+    menubar: &EmuMenubar,
+    accel_group: &AccelGroup,
+    ui_to_gb: &Sender<UiAction>
+) {
+    let window = window.clone();
+    let ui_to_gb = ui_to_gb.clone();
+    menubar.open_btn.connect_activate(move |_| {
+        let filename = show_open_dialog(&window);
+        if let Some(f) = filename {
+            ui_to_gb.send(UiAction::Load(f)).unwrap();
+        }
+    });
+    let (open_key, open_modifier) = gtk::accelerator_parse("<Primary>O");
+    menubar.open_btn.add_accelerator("activate", accel_group, open_key, open_modifier, AccelFlags::VISIBLE);
+}
+
+fn connect_keypress(window: &ApplicationWindow, ui_to_gb: &Sender<UiAction>) {
+    let window = window.clone();
+    let ui_to_gb = ui_to_gb.clone();
+    window.connect_key_press_event(move|_, evt| {
+        let mut key = evt.get_keyval();
+        *key = gdk::keyval_to_upper(*key);
+        if let Some(btn) = key2btn(key) {
+            ui_to_gb.send(UiAction::BtnPress(btn)).unwrap();
+        }
+
+        Inhibit(false)
+    });
+}
+
+fn connect_keyrelease(window: &ApplicationWindow, ui_to_gb: &Sender<UiAction>) {
+    let window = window.clone();
+    let ui_to_gb = ui_to_gb.clone();
+    window.connect_key_release_event(move|_, evt| {
+        let mut key = evt.get_keyval();
+        *key = gdk::keyval_to_upper(*key);
+        if let Some(btn) = key2btn(key) {
+            ui_to_gb.send(UiAction::BtnRelease(btn)).unwrap();
+        }
+
+        Inhibit(false)
+    });
+}
+
+fn show_open_dialog(parent: &ApplicationWindow) -> Option<PathBuf> {
+    let mut file = None;
+    let dialog = FileChooserDialog::new(Some("Select a Game Boy ROM"), Some(parent), FileChooserAction::Open);
+    let filter = FileFilter::new();
+    filter.add_mime_type("application/x-gameboy-rom");
+    filter.add_mime_type("application/x-gameboy-color-rom");
+    filter.set_name(Some("Game Boy ROM files"));
+    dialog.add_filter(&filter);
+    dialog.add_buttons(&[
+        ("Open", gtk::ResponseType::Ok),
+        ("Cancel", gtk::ResponseType::Cancel),
+    ]);
+
+    let result = dialog.run();
+    if result == gtk::ResponseType::Ok {
+        file = dialog.get_filename();
+    }
+    dialog.close();
+
+    file
+}
+
+/// ```
+/// Key to Button
+///
+/// Converts keycode into GameBoy button
+///
+/// Input:
+///     GDK keybode keycode (Key)
+///
+/// Output:
+///     Gameboy button (Option<Buttons>)
+/// ```
+fn key2btn(key: Key) -> Option<Buttons> {
+    match key {
+        gdk::keys::constants::Down =>    Some(Buttons::Down),
+        gdk::keys::constants::Up =>      Some(Buttons::Up),
+        gdk::keys::constants::Right =>   Some(Buttons::Right),
+        gdk::keys::constants::Left =>    Some(Buttons::Left),
+        gdk::keys::constants::Return =>  Some(Buttons::Start),
+        gdk::keys::constants::Back =>    Some(Buttons::Select),
+        gdk::keys::constants::X =>       Some(Buttons::A),
+        gdk::keys::constants::Z =>       Some(Buttons::B),
+        _ =>                             None
+    }
+}
