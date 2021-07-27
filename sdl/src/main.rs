1f1aa2ee9cc6a0407057e4f397649ada370e52bf
aquova <abrick912@gmail.com>
2021-07-27 11:48:59 -0500

    Simplify SDL rendering function

--- sdl/src/main.rs
+++ sdl/src/main.rs
@@ -3,11 +3,12 @@
 
 use songbird_core::cpu::Cpu;
 use songbird_core::io::Buttons;
-use songbird_core::utils::{DISP_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH};
+use songbird_core::utils::{COLOR_CHANNELS, DISP_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH};
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
@@ -17,7 +18,9 @@ use std::io::prelude::*;
 use std::io::Read;
 
 // Constants
-const SCALE: usize = 5;
+const SCALE: u32 = 5;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 
 pub fn main() {
     let args: Vec<_> = env::args().collect();
@@ -37,7 +40,7 @@ pub fn main() {
     // Set up SDL
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window(title, (SCALE * SCREEN_WIDTH) as u32, (SCALE * SCREEN_HEIGHT) as u32).position_centered().opengl().build().unwrap();
+    let window = video_subsystem.window(title, WINDOW_WIDTH, WINDOW_HEIGHT).position_centered().opengl().build().unwrap();
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
     canvas.present();
@@ -96,17 +99,15 @@ pub fn main() {
 ///     SDL2 Canvas (Canvas<Window>)
 /// ```
 fn draw_screen(data: &[u8; DISP_SIZE], canvas: &mut Canvas<Window>) {
-    canvas.set_scale(SCALE as f32, SCALE as f32).unwrap();
+    for i in (0..DISP_SIZE).step_by(COLOR_CHANNELS) {
+        canvas.set_draw_color(Color::RGB(data[i], data[i + 1], data[i + 2]));
+        let pixel = i / COLOR_CHANNELS;
+        let x = (pixel % SCREEN_WIDTH) as u32;
+        let y = (pixel / SCREEN_WIDTH) as u32;
 
-    let texture_creator = canvas.texture_creator();
-    let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::RGBA32, SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32).unwrap();
-    texture.with_lock(None, |buffer: &mut [u8], _: usize| {
-        for i in 0..data.len() {
-            buffer[i] = data[i];
-        }
-    }).unwrap();
-
-    canvas.copy(&texture, None, None).unwrap();
+        let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+        canvas.fill_rect(rect).unwrap();
+    }
     canvas.present();
 }
 
